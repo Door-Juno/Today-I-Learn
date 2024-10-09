19983040131ef795d5fefb8252bf900b60d47989
Junho Moon <bublman3375@knu.ac.kr>
2024-10-09 15:59:46 +0900

    TIL

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 Today I Learn ( 24.10.08~ ) 
 경북대 22학번 복수전공 개발자 지망생 공부 끄적이는 페이지입니다
 
-24.10.09 TIL
-[How to use github - basic](https://github.com/Door-Juno/Today-I-Learn/blob/main/TIL/Git/github.md)
+24.10.09 TIL  
+[How to use github - basic](https://github.com/Door-Juno/Today-I-Learn/blob/main/TIL/Git/github.md)  
+[Bitwise 연습문제](https://github.com/Door-Juno/Today-I-Learn/blob/main/TIL/C/Bitwise%20exercise.md)  
 
-24.10.08 TIL 
+24.10.08 TIL   
 [Bitwise Operation](https://github.com/Door-Juno/Today-I-Learn/blob/main/TIL/C/Bitwise%20Operation.md)
 
--- TIL/C/Bitwise.c
+++ TIL/C/Bitwise.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <math.h>
+#define true 1
+#define false 0
+typedef unsigned long long long_t ;
 
 // Lab04-4
 void check_students(char a, char b, char c){
@@ -9,6 +13,7 @@ void check_students(char a, char b, char c){
     printf("%07x\n", x);
 }
 
+// Lab04-05
 void hd(char a, char b){
     unsigned char c = a ^ b ; 
     int hd = 0;
@@ -18,8 +23,43 @@ void hd(char a, char b){
     printf("%d",hd);
 }
 
+//lab06-02
+void bitcount_two(long_t N){
+    int leading = true , one = 0 , zero = 0 ;
+    for ( int i = 63 ; i >= 0 ; i --){
+        int bit =  (N>>i)&1;
+        if(leading && bit == 0 ) 
+            continue ;
+        leading = false ; 
+        (bit) ? one ++ : zero ++ ;
+    }
+    printf("%d\n", one);
+    printf("%d\n", zero);
+}
+
+//Lab06-03
+void bitcount_three(long_t N){
+    int maxCount = 0 , curCount=0;
+
+   while( N != 0 ){
+        if( N & 1 ) {
+            curCount++;
+            if(curCount > maxCount){
+                maxCount = curCount;
+            }   
+        }
+        else{
+            curCount = 0 ;
+        }
+        N >>= 1 ;
+    }
+    printf("%d\n",maxCount);
+}
+
 
 int main() {
     check_students('A','B','C');
-
+    hd('A','B');
+    bitcount_two(33);
+    bitcount_three(222);
 }
